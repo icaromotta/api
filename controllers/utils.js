aa6329dcec79ae8e185eb5320698e78a5e473015
Ícaro Costa <icarocmotta@gmail.com>
2020-02-21 14:13:47 -0300

    adjusts in endpoints

--- controllers/utils.js
+++ controllers/utils.js
@@ -3,6 +3,6 @@ const Contact = mongoose.model('Contact')
 
 module.exports.contact = (req, res) => {
   Contact.create(req.body, (err, message) => {
-    return res.status(200).send({ ok: true })
+    res.send({ ok: true })
   })
 }
