4b84f43b7afbe68e757c55e2de24928db322562a
Ícaro Costa <icarocmotta@gmail.com>
2020-02-25 12:11:53 -0300

    auth normalized

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,27 @@ const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const { sign, verify } = require('../config/jwt')
 
+const authMiddleware = async (req, res, next) => {
+
+  const [token] = req.headers.authorization.split(' ')
+  
+  try {
+
+    const payload = await verify(token)
+    const user = await User.findById(payload.user)
+
+    req.auth = user
+
+    if(!user) {
+      res.send(401)
+    }
+
+    next()
+  } catch (error) {
+    res.send(error)
+  }
+}
+
 router.post('/create', (req, res) => {
 
   // TODO: Mover codigo pra controller
@@ -25,11 +46,14 @@ router.post('/create', (req, res) => {
       token
     })
   });
-});
+})
 
-router.post('/login', (req, res) => {
+router.get('/login', (req, res) => {
 
-  User.findOne({ email: req.body.email }, (err, user) => {
+  const [hashType, hash] = req.headers.authorization.split(' ')
+  const [email, password] = Buffer.from(hash, 'base64').toString().split(':')
+  
+  User.findOne({ email: email }, (err, user) => {
 
     if (!user) {
       res.status(400).send({
@@ -37,12 +61,28 @@ router.post('/login', (req, res) => {
       })
     }
 
-    if (user.password === req.body.password) {
-      return res.status(200).send({ ok: true })
+    const token = sign({ user: user.id })
+
+    if (user.password === password) {
+      return res.status(200).send({ user, token })
     } else {
       return res.status(200).json({ ok: false })
     }
   })
-});
+})
+
+router.get('/users', authMiddleware, async (req, res) => {
+
+  try {
+    const users = await User.find()
+    res.send(users)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.get('/me', authMiddleware, (req, res) => {
+  res.send(req.auth)
+})
 
 module.exports = router;
